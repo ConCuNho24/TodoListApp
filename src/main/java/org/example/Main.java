b4183e99ed7abbf4ca523dc59c524b51b6c3f784
concunhonho <anhquanhandsome2408@gmail.com>
2026-03-06 13:28:28 +1000

    Set up initial project structure

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,87 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    private static ArrayList<User> users = new ArrayList<>();
+
+    private static IAuthenticationService authService = new IAuthenticationService() {
+        @Override
+        public User signUp(String username, String password) {
+            return null;
+        }
+
+        @Override
+        public User logIn(String username, String password) {
+            return users.get(0);
+        }
+    };
+
+    private static boolean isRunning = true;
+
+    public static void main(String[] args) {
+        users.add(new User("test", "test"));
+
+        while (isRunning) {
+            showMenu();
+        }
+    }
+
+    public static void showMenu() {
+        System.out.println("Welcome to the To-Do List Application!");
+        System.out.println("1. Log in");
+        System.out.println("2. Sign up");
+        System.out.println("3. Exit");
+        System.out.print("Enter your choice: ");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        handleMenu(choice);
+    }
+
+    public static void handleMenu(int choice) {
+        switch (choice) {
+            case 1:
+                onLogIn();
+                break;
+            case 2:
+                onSignUp();
+                break;
+            case 3:
+                onExit();
+                break;
+            default:
+                System.out.println("Invalid choice!");
+        }
+    }
+
+    public static void onLogIn() {
+        System.out.print("Enter your username: ");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+
+        System.out.print("Enter your password: ");
+        String password = scanner.nextLine();
+
+        User user = authService.logIn(username, password);
+        System.out.println("Welcome, " + user.getUsername() + "!");
+        // TODO Later: Add the to-do list operations
+    }
+
+    public static void onSignUp() {
+        System.out.print("Enter your username: ");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+
+        System.out.print("Enter your password: ");
+        String password = scanner.nextLine();
+
+        User user = authService.signUp(username, password);
+        // TODO Later: Shows a message based on the result
+    }
+
+    public static void onExit() {
+        isRunning = false;
+    }
+}
